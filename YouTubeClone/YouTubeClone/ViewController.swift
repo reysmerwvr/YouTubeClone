456ebeab36d903be08ddfc90f362dc7a94372575
Reysmer Valle <reysmerwvr@gmail.com>
2022-05-22 23:01:10 +0200

    feat: add project assets

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Reysmer Valle on 5/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
